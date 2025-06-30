b175d6ca0ab194f102b1d4ca85ab60608479c449
root <root@DESKTOP-PPTI2QP.>
2025-06-30 14:52:45 +0900

    update script.sh

--- .github/script.sh
+++ .github/script.sh
@@ -6,8 +6,11 @@ namespace=$3
 
 deploy=`oc get deployment $app_name`
 if [[ "$?" -eq 0 ]]; then
-    oc set image deployment/$app_name $app_name=$image
-    oc rollout restart deployment/$app_name
+    oc set image deployment/$app_name $app_name=$image -n $namespace
+    oc patch deployment $app_name -n $namespace --type=merge -p '{"spec": {"template": {"spec": {"serviceAccount": "default", "securityContext": {"runAsUser": "0"}}}}}'
+    oc rollout restart deployment/$app_name -n $namespace
 else
     oc new-app $image --name $app_name -n $namespace
+    oc patch deployment $app_name -n $namespace --type=merge -p '{"spec": {"template": {"spec": {"serviceAccount": "default", "securityContext": {"runAsUser": "0"}}}}}'
+    oc rollout restart deployment/$app_name -n $namespace
 fi
